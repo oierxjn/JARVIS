287790e132f6bc7ca8b4afee6d6b5ad00fad1ecf
oierxjn <1934672944@qq.com>
2025-12-13 14:25:23 +0800

    Refactor: 提取Home界面组件
    优化代码结构

--- app/src/main/java/org/oierxjn/jarvis/MainActivity.kt
+++ app/src/main/java/org/oierxjn/jarvis/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import org.oierxjn.jarvis.ui.Home
 import org.oierxjn.jarvis.ui.theme.JARVISTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,30 +19,9 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            JARVISTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            Home()
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    JARVISTheme {
-        Greeting("Android")
-    }
-}
---
+++ app/src/main/java/org/oierxjn/jarvis/ui/Home.kt
@@ -0,0 +1,29 @@
+package org.oierxjn.jarvis.ui
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import org.oierxjn.jarvis.ui.theme.JARVISTheme
+
+@Composable
+fun Home() {
+    JARVISTheme {
+        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+            Greeting(
+                name = "Android",
+                modifier = Modifier.padding(innerPadding)
+            )
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
